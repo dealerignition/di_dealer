5e35a29eadb5b16279aa10c610b7fb95a0a215d8
Luke Seelenbinder <luke.seelenbinder@gmail.com>
2012-11-01 19:35:39 +0000

    Cleanup dealer.rb.

--- lib/dealer/dealer.rb
+++ lib/dealer/dealer.rb
@@ -2,28 +2,23 @@ class Dealer
   include ActiveModel::Conversion
   extend ActiveModel::Naming
 
-  attr_accessor :address_1, :address_2, :city, :id, :name, :phone, :state, :website, :zipcode, :active_catalogs, :latitude, :longitude
+  ATTRIBUTES = [:address_1, :address_2, :city, :id, :name, :phone, :state, :website, :zipcode, :active_catalogs, :active_promotions, :latitude, :longitude]
+  ATTRIBUTES.each { |a| attr_accessor a }
 
-  def self.find(id)
-    Rails.cache.fetch :dealer => id do
-       dealer = JSON.parse(Nestful.get(DIDealer.options[:url] + "/#{id}.json", :headers => DIDealer.api_headers))
+  def self.find(id, brand = nil)
+    Rails.cache.fetch :dealer => id, :brand => brand do
+       dealer = JSON.parse(Nestful.get(
+         DIDealer.options[:url] + "/#{id}.json" + (brand.nil? ? "" : "&brand=#{ brand }"),
+         :headers => DIDealer.api_headers))
        Dealer.new(dealer)
     end
   end
 
   def initialize(dealer)
-    @id = dealer["id"]
-    @name = dealer["name"]
-    @address_1 = dealer["address_1"]
-    @address_2 = dealer["address_2"]
-    @city = dealer["city"]
-    @state = dealer["state"]
-    @zipcode = dealer["zipcode"]
-    @phone = dealer["phone"]
-    @website = dealer["website"]
-    @active_catalogs = dealer["active_catalogs"]
-    @latitude = dealer["latitude"]
-    @longitude = dealer["longitude"]
+    ATTRIBUTES.each do |a|
+      self.instance_variable_set "@#{a}", dealer[a.to_s]
+    end
+
     self
   end
 
